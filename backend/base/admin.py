a0229a1ba0a1eab4806cb8c5259d26a5e2e32808
Rankonana Mokoena <rankonanamokoena@gmail.com>
2023-05-24 03:27:19 +0200

    added the wireframes

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -4,6 +4,7 @@ from .models import *
 admin.site.register(UserProfile)
 admin.site.register(Car)
 admin.site.register(WareHouse)
+admin.site.register(RequestPickupImages)
 admin.site.register(RequestPickup)
 admin.site.register(Pickup)
 admin.site.register(PickupMessage)
